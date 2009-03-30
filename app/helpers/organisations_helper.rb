168809b0b0dbad3d902a5f8433ccf9b13dd3b10a
robmckinnon <rob@theyworkforyou.co.nz>
2009-03-30 20:25:23 +0100

    populate_company_number when organisation saved or updated

--- app/helpers/organisations_helper.rb
+++ app/helpers/organisations_helper.rb
@@ -1,2 +1,17 @@
 module OrganisationsHelper
+
+  def search_script element_id, name
+%Q|<script src="http://www.google.com/jsapi" type="text/javascript"></script>
+  <script language="Javascript" type="text/javascript">
+  google.load('search', '1');
+  function OnLoad() {
+    var searchControl = new google.search.SearchControl();
+    var localSearch = new google.search.WebSearch();
+    searchControl.addSearcher(localSearch);
+    searchControl.draw(document.getElementById("#{element_id}"));
+    searchControl.execute("#{name} site:wikipedia.org");
+  }
+  google.setOnLoadCallback(OnLoad);
+</script>|
+  end
 end
--- app/models/organisation.rb
+++ app/models/organisation.rb
@@ -1,4 +1,7 @@
 require 'acts_as_wikipedia'
+require 'open-uri'
+require 'uri'
+require 'morph'
 
 class Organisation < ActiveRecord::Base
 
@@ -18,4 +21,22 @@ class Organisation < ActiveRecord::Base
 
   validates_presence_of :url
   validates_uniqueness_of :url
+
+  before_validation :populate_company_number
+
+  private
+    def populate_company_number
+      encoded_name = URI.encode name.gsub('&','AND')
+      url = "http://localhost:3001/search?q=#{encoded_name}&f=xml"
+      xml = open(url).read.gsub("id>","companies_revealed_id>")
+      hash = Hash.from_xml(xml)
+      results = Morph.from_hash(hash)
+      y results
+      if results.result_size.to_i == 0
+      elsif results.result_size.to_i == 1
+        self.company_number = results.company.company_number
+      elsif results.result_size.to_i > 1
+      end
+      puts results.result_size
+    end
 end
