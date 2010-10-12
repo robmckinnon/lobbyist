3effa974867058a1a05b680fb255b4884678aba5
robmckinnon <rob@theyworkforyou.co.nz>
2010-10-12 12:57:40 +0100

    added new date

--- app/controllers/scorecard_controller.rb
+++ app/controllers/scorecard_controller.rb
@@ -5,6 +5,8 @@ class ScorecardController < ActionController::Base
   def show
     if params[:date] == '2010-09-12'
       render 'scorecard/2010_09_12', :layout => false
+    elsif params[:date] == '2010-10-07'
+      render 'scorecard/2010_10_07', :layout => false
     elsif params[:date].nil?
       render 'scorecard/show'
     else
